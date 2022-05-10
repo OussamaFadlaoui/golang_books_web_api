620d56f266e7cb69ca2834faecb07b09a5655d98
Oussama Fadlaoui <me@oussamafadlaoui.com>
2022-05-10 18:27:00 +0200

    Fix content-type middleware

--- middlewares/content_type_json_middleware.go
+++ middlewares/content_type_json_middleware.go
@@ -1,14 +1,12 @@
 package middlewares
 
 import (
-	"fmt"
 	"net/http"
-	"time"
 )
 
 func ContentTypeJsonMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(fmt.Sprintf("[%v] %v", time.Now().Format("2006-01-02 15:04:10"), r.RequestURI))
+		w.Header().Add("Content-Type", "application/json")
 		next.ServeHTTP(w, r)
 	})
 }
